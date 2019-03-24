720b6b99fc5aa52eb9616c9a974f9ee8782f87c4
Hemu <hemanthm2277@gmail.com>
2019-03-24 16:03:28 -0400

    Created the repository

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Engagement quantification while interacting with a haptic device under different control stratgies',
+    author='Hemanth Manjunathaa',
+    license='MIT',
+)
