d0f4eb6ea7948af295155696a37f3f00ad7307bf
Hemu <hemanthm2277@gmail.com>
2019-04-03 21:55:51 -0400

    visualize.R update

--- src/visualization/visualize.R
+++ src/visualization/visualize.R
@@ -9,12 +9,11 @@ config <- yaml.load_file('./src/config.yml')
 r_dataset_path <- paste('./',config$r_dataset, sep='')
 df <- read_feather(r_dataset_path)
 
-response <- df$beta_alpha_theta
-control <- df$control_type
-
-my_comparisons <- list( c("no_force", "error_augmentation"), 
+comparison_list <- list( c("no_force", "error_augmentation"), 
                         c("no_force", "error_reduction"), 
                         c("error_reduction", "error_augmentation") )
 ggboxplot(df, x = 'control_type', y = 'beta_alpha_theta',
-          color = 'control_type', palette = "jco")+ 
-  stat_compare_means(comparisons = my_comparisons, label.y = c(0.05, 0.125, 0.175)+max(response))
+          fill = 'control_type', palette = 'jco', bxp.errorbar=TRUE, 
+          bxp.errorbar.width = 0.25, width = 0.4)+ 
+  stat_compare_means(comparisons = comparison_list, 
+                     label.y = c(0.05, 0.125, 0.175)+max(df$beta_alpha_theta))
