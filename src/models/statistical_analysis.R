25b6bb79aab951a0458c5145e4bf44988424eef9
Hemu <hemanthm2277@gmail.com>
2019-04-08 13:16:54 -0400

    Statistical analysis update

--- src/models/statistical_analysis.R
+++ src/models/statistical_analysis.R
@@ -1,14 +1,17 @@
 library(yaml)
 library(feather)
-library(lmerTest)
+library(lme4)
+library(simr)
 
 # Read the configuration path
-config <- yaml.load_file('./src/config.yml')
+config <- yaml.load_file("./src/config.yml")
 
+#-----------------------------------------------------------------------
 # Read the r dataset
-r_dataset_path <- paste('./',config$r_dataset, sep='')
+r_dataset_path <- paste("./", config$r_dataset, sep = "")
 df <- read_feather(r_dataset_path)
-
+df <- na.omit(df)
+#-----------------------------------------------------------------------
 # As factor
 df$control_type <- as.factor(df$control_type)
 df$hand_type <- as.factor(df$hand_type)
@@ -18,19 +21,26 @@ df$subject <- as.factor(df$subject)
 response <- df$beta_alpha_theta
 
 #-----------------------------------------------------------------------
-# Prior analysis 
-engagement <- lmer(response ~ 1 + (1|df$hand_type),
-                   data = df)
+# Prior analysis
+engagement <- lmer(response ~ 1 + (1 | df$hand_type),
+  data = df
+)
 summary(engagement)
 
 #-----------------------------------------------------------------------
 # Linear mixed models with hand as random effect
-# Set reference level 
+# Set reference level
 df$control_type <- relevel(df$control_type, "no_force")
 df$hand_type[0]
 df$control_type[0]
 
+control_type <- df$control_type
 # Linear mixed models
-engagement <- lmer(response ~ df$control_type + (1|df$subject) + (1|df$hand_type),
-                   data = df)
-summary(engagement)
+fit.model <- lmer(response ~ factor + (1 | df$subject) + (1 | df$hand_type), 
+                         data = df)
+summary(fit.model)
+#-----------------------------------------------------------------------
+# Power analysis?
+power <- powerSim(fit.model, nsim = 10)
+power
+lastResult()$errors
--- src/visualization/visualize.R
+++ src/visualization/visualize.R
@@ -1,5 +1,6 @@
 library(yaml)
 library(feather)
+library(ggplot2)
 library(ggpubr)
 
 # Read the configuration path
@@ -12,8 +13,9 @@ df <- read_feather(r_dataset_path)
 comparison_list <- list( c("no_force", "error_augmentation"), 
                         c("no_force", "error_reduction"), 
                         c("error_reduction", "error_augmentation") )
-ggboxplot(df, x = 'control_type', y = 'beta_alpha_theta',
+p <- ggboxplot(df, x = 'control_type', y = 'beta_alpha_theta',
           fill = 'control_type', palette = 'jco', bxp.errorbar=TRUE, 
           bxp.errorbar.width = 0.25, width = 0.4)+ 
   stat_compare_means(comparisons = comparison_list, 
                      label.y = c(0.05, 0.125, 0.175)+max(df$beta_alpha_theta))
+p 
